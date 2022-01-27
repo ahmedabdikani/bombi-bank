0a2d5a4f27753dd1256dde7704e4d483cd2c46c0
DecovicHunter <ahmed_abdikani@hotmail.com>
2022-01-27 14:33:24 +0800

    prioritize desktop nav for less layout shifts

--- landing-page/components/shared/Header.tsx
+++ landing-page/components/shared/Header.tsx
@@ -26,59 +26,60 @@ const pageLinks = ['Bank', 'About', 'Open Account', 'Card', 'Online'];
 const Header: React.FC<HeaderProps> = ({}) => {
   const router = useRouter();
   const { onToggle } = useDrawerContext();
-  const isMobile = useScreenSize().width < 800;
+  const isDesktop = useScreenSize().width > 800;
 
-  if (isMobile) {
+  if (isDesktop) {
     return (
-      <HStack p='4' justify='space-between'>
-        <Logo />
-        <Button onClick={onToggle}>
-          <Image width={28} height={28} src='/images/menu.png' alt='menu' />
-        </Button>
-      </HStack>
+      <Wrapper>
+        <HStack spacing='8'>
+          <VStack align='stretch' flex={1} py='4' spacing={0}>
+            <HStack spacing='8'>
+              {routes.map(({ name, path }, index) => {
+                const isActive = router.pathname === path;
+                return (
+                  <NextLink href={path} key={index} passHref>
+                    <Link
+                      pb='4'
+                      borderBottomColor='brand.1'
+                      borderBottomWidth={isActive ? '3px' : '0px'}
+                    >
+                      <Caption
+                        fontWeight='semibold'
+                        color={isActive ? 'black' : 'gray'}
+                      >
+                        {name}
+                      </Caption>
+                    </Link>
+                  </NextLink>
+                );
+              })}
+            </HStack>
+            <Divider />
+            <HStack justify='space-between' pt='4'>
+              <Logo />
+              <HStack spacing='4'>
+                {pageLinks.map((link) => (
+                  <Link key={link} href='#'>
+                    <Body1 fontWeight='medium'>{link}</Body1>
+                  </Link>
+                ))}
+              </HStack>
+              <HStack />
+            </HStack>
+          </VStack>
+          <PrimaryButton>Sign in</PrimaryButton>
+        </HStack>
+      </Wrapper>
     );
   }
+
   return (
-    <Wrapper>
-      <HStack spacing='8'>
-        <VStack align='stretch' flex={1} py='4' spacing={0}>
-          <HStack spacing='8'>
-            {routes.map(({ name, path }, index) => {
-              const isActive = router.pathname === path;
-              return (
-                <NextLink href={path} key={index} passHref>
-                  <Link
-                    pb='4'
-                    borderBottomColor='brand.1'
-                    borderBottomWidth={isActive ? '3px' : '0px'}
-                  >
-                    <Caption
-                      fontWeight='semibold'
-                      color={isActive ? 'black' : 'gray'}
-                    >
-                      {name}
-                    </Caption>
-                  </Link>
-                </NextLink>
-              );
-            })}
-          </HStack>
-          <Divider />
-          <HStack justify='space-between' pt='4'>
-            <Logo />
-            <HStack spacing='4'>
-              {pageLinks.map((link) => (
-                <Link key={link} href='#'>
-                  <Body1 fontWeight='medium'>{link}</Body1>
-                </Link>
-              ))}
-            </HStack>
-            <HStack />
-          </HStack>
-        </VStack>
-        <PrimaryButton>Sign in</PrimaryButton>
-      </HStack>
-    </Wrapper>
+    <HStack p='4' justify='space-between'>
+      <Logo />
+      <Button onClick={onToggle}>
+        <Image width={28} height={28} src='/images/menu.png' alt='menu' />
+      </Button>
+    </HStack>
   );
 };
 export default Header;
