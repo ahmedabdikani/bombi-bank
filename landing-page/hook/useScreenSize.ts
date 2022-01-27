fe4e9c166cc2a2826330928b9ab2f8726b7d28b4
DecovicHunter <ahmed_abdikani@hotmail.com>
2022-01-27 10:32:54 +0800

    using layouteffect to get intail window size

--- landing-page/hook/useScreenSize.ts
+++ landing-page/hook/useScreenSize.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useLayoutEffect, useState } from 'react';
 
 const useScreenSize = () => {
   const [screenSize, setScreenSize] = useState<{
@@ -6,22 +6,22 @@ const useScreenSize = () => {
     width: number;
   }>({
     height: 0,
-    width: 0
+    width: 0,
   });
-  useEffect(() => {
-    const handleResize = () => 
+  useLayoutEffect(() => {
+    const handleResize = () =>
       setScreenSize({
         height: window.innerHeight,
         width: window.innerWidth,
-      })
+      });
 
-      handleResize();
-    window.addEventListener("resize", handleResize);
+    handleResize();
+    window.addEventListener('resize', handleResize);
     return () => {
-      window.removeEventListener("resize", handleResize)
+      window.removeEventListener('resize', handleResize);
     };
-  },[]);
+  }, []);
   return screenSize;
-}
+};
 
 export default useScreenSize;
